3c1883b6ce1b821760a432a1b85d12f9d8b6d596
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-27 09:25:49 -0600

    Wrote test based on User Story. Working on getting capybara to delete the text that is in the search box and replace it with the zip code.

---
+++ spec/features/user_can_search_for_alt_fuel_stations_by_zip_code_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'User Can Search by Zip Code', type: :feature do
+  describe 'As a visitor' do
+    describe 'when I visit /' do
+      describe 'and I fill in the search form and click on Locate' do
+        it 'should take me to /search' do
+          visit '/'
+          fill_in 'q', with: '80206'
+          save_and_open_page
+        end
+
+        it 'I should see the total results of the stations that match my query' do
+        end
+
+        it 'I should see a list of the 15 closest stations within 5 miles sorted by distance' do
+        end
+
+        it 'should only show stations that are Electric and Propane' do
+        end
+
+        it 'should only show the stations that are public, and not private, planned or temporarily unavailable' do
+        end
+
+        it 'should show the station Name, Address, Fuel Types, Distance, and Access Times' do
+        end
+
+      end
+    end
+  end
+end
+
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80206 (Note: Use the existing search form)
+# And I click "Locate"
+# Then I should be on page "/search"
+# Then I should see the total results of the stations that match my query.
+# Then I should see a list of the 15 closest stations within 5 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And the stations should only be public, and not private, planned or temporarily unavailable.
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
