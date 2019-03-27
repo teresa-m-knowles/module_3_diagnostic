483d4002eab23afa591cd2daf86fe86394e56c58
Teresa Knowles <teresa07moreno@gmail.com>
2019-03-27 10:20:28 -0600

    Adds facade test

---
+++ spec/facades/search_result_facade_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe MemberSearchResultFacade do
+  it 'exists'do
+    zip = '80206'
+    result_facade = SearchResultFacade.new(zip)
+
+    expect(result_facade).to be_a(SearchResultFacade)
+  end
+
+  describe "instance methods" do
+    describe "#stations_count" do
+      it "returns the count of members" do
+        zip = '80206'
+        result_facade = SearchResultFacade.new(zip)
+
+        expect(result_facade.stations_count).to eq(15)
+
+      end
+    end
+
+  end
+end
